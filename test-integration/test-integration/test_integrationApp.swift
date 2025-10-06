948ee35e67fa7629d3ef77dc8c001e56aefa5f25
destucr <destucr@gmail.com>
2025-10-06 11:38:58 +0700

    init commit

---
+++ test-integration/test-integration/test_integrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_integrationApp.swift
+//  test-integration
+//
+//  Created by Destu Cikal Ramdani on 06/10/25.
+//
+
+import SwiftUI
+
+@main
+struct test_integrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
