70a865fe5de39bed85365c5a449b570327407f2c
destucr <destucr@gmail.com>
2025-10-06 11:42:56 +0700

    Update: contentview project

--- test-integration/test-integration/ContentView.swift
+++ test-integration/test-integration/ContentView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var backgroundColor = Color.white
+    let colors: [Color] = [.red, .green, .blue, .yellow, .purple, .orange]
+
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -16,6 +20,11 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .contentShape(Rectangle())
+        .onTapGesture {
+            backgroundColor = colors.randomElement() ?? .white
+        }
+        .background(backgroundColor)
     }
 }
 
